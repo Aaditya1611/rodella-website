c68cce4d3984cf7a6dfc04bfe2172a4564917fdd
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-04-09 21:34:56 +0530

    remove the aircraft page link and other dummy products

--- rodella-website/src/constants/NavItems.jsx
+++ rodella-website/src/constants/NavItems.jsx
@@ -4,45 +4,45 @@ export const Menus = [
   {
     id: 1,
     name: "Aircrafts",
-    subMenuHeading: ["Multirotor", "FixedWings",],
-    href: "/aircrafts",
+    subMenuHeading: ["", "",],
+    //href: "/aircrafts",
     subMenu: [
-      {
-        id: 1,
-        name: "X-MQ650LV",
-        desc: "Responsive design",
-        href: "/multirotor/1"
-      },
-      {
-        id: 2,
-        name: "Project Talon-Q9872MV",
-        desc: "Management content",
-        href: "/fixedwing/1"
-      },
-      {
-        id: 3,
-        name: "MQ750LV",
-        desc: "Site control",
-        href: "/multirotor/2"
-      },
-      {
-        id: 4,
-        name: "Nova Wing-Q3484MXV",
-        desc: "CMS",
-        href: "/fixedwing/2"
-      },
-      {
+      // {
+      //   id: 1,
+      //   name: "X-MQ650LV",
+      //   desc: "Responsive design",
+      //   href: "/multirotor/1"
+      // },
+      // {
+      //   id: 2,
+      //   name: "Project Talon-Q9872MV",
+      //   desc: "Management content",
+      //   href: "/fixedwing/1"
+      // },
+      // {
+      //   id: 3,
+      //   name: "MQ750LV",
+      //   desc: "Site control",
+      //   href: "/multirotor/2"
+      // },
+      // {
+      //   id: 4,
+      //   name: "Nova Wing-Q3484MXV",
+      //   desc: "CMS",
+      //   href: "/fixedwing/2"
+      // },
+       {
         id: 6,
         name: "SAP-6",
         desc: "Multirotor",
         href: "/othercrafts/1"
       },
-      {
-        id: 5,
-        name: "X-MQ900",
-        desc: "Link pages",
-        href: "/multirotor/3"
-      },
+      // {
+      //   id: 5,
+      //   name: "X-MQ900",
+      //   desc: "Link pages",
+      //   href: "/multirotor/3"
+      // },
       {
         id: 9,
         name: "SAP-9",
