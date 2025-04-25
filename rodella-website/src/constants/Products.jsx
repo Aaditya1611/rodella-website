0e689fdbf546f4641b3661dbc9ff51a61f851181
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-04-25 23:54:22 +0530

    add new product

--- rodella-website/src/constants/Products.jsx
+++ rodella-website/src/constants/Products.jsx
@@ -7,9 +7,6 @@ import S9Isofront from "./../assets/images/S9front.svg";
 import S9Isoside from "./../assets/images/S9side.svg";
 import S9Isometric from "./../assets/images/S9isometric.svg";
 import sap900 from "./../assets/images/Sap9.png";
-import sap91 from "./../assets/images/SAP-9/front.svg";
-import sap92 from "./../assets/images/SAP-9/isometric.svg";
-import sap93 from "./../assets/images/SAP-9/side.svg";
 
 const Products = [
 
@@ -48,6 +45,39 @@ const Products = [
 
     {
         id: 2,
+        name: "SAP-7",
+        description: "SAP-6 is a light weight micro-category variant borrowing its structural toughness ffrom MQ750+  Developed for quick deployment and versatile operations in the coldest and the windiest of the weathers,  the MQ650LV delivers 40 mins of operational endurance with a 10x optical zoom equipped HD electro-optic sensor. Swappable battery options make re-deployment swift enhancing the system's efficiency in real time survielance missions. Rapid survielance and reconnaisance in the tactile levels of warfare are most easily and effectively achieved with MQ650LV.",
+
+        FlightTime: "40 mins",
+        TransmissionRange: "10km",
+        Camera: "4k, 10x Optical zoom",
+        Payload: "300 gms",
+
+        TechnicalDetail1: "Cruise Speed",
+        TechnicalDetail2: "Wind resistence index",
+        TechnicalDetail3: "Gimbal Stabilization",
+        TechnicalDetail4: "Dash Speed",
+        TechnicalDetail5: "Operational Altitude",
+        TechnicalDetail6: "Wheel Base",
+        TechnicalDetail7: "Empty Weight",
+
+        TechnicalDetailsValue1: "8 m/s",
+        TechnicalDetailsValue2: "30 knots",
+        TechnicalDetailsValue3: "3 aixs stabilized",
+        TechnicalDetailsValue4: "10 m/s",
+        TechnicalDetailsValue5: "500mAGL",
+        TechnicalDetailsValue6: "650 mm",
+        TechnicalDetailsValue7: "1kg",
+
+        MainBGImg: MQ6501,
+        OtherImg: MQ6502,
+        IsoFront: S6Isofront,
+        IsoTop: S6Isoside,
+        Isometric: S6Isometric
+    },
+
+    {
+        id: 3,
         name: "SAP-9",
         description: "SAP-9 is a light weight micro-category variant borrowing its structural toughness from MQ750+  Developed for quick deployment and versatile operations in the coldest and the windiest of the weathers,  the MQ650LV delivers 40 mins of operational endurance with a 10x optical zoom equipped HD electro-optic sensor. Swappable battery options make re-deployment swift enhancing the system's efficiency in real time survielance missions. Rapid survielance and reconnaisance in the tactile levels of warfare are most easily and effectively achieved with MQ650LV.",
 
